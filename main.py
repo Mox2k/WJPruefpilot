2d6d71818c6ee870e5f70a1afca6d95dde46cb1e
Lennart Joehnk <lennart.joehnk@gmail.com>
2024-07-21 20:04:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from main_window import MainWindow
+
+
+def main():
+    """Hauptfunktion zum Starten der Anwendung."""
+    root = tk.Tk()
+    root.title("Prüfpilot")
+    root.iconphoto(True, tk.PhotoImage(file="WJ-Icon.png"))
+    app = MainWindow(root)
+    app.pack(fill=tk.BOTH, expand=True)
+    root.geometry("1000x451")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
