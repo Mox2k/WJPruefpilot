84dce55ec5be7b6f593467d841f0424f57fb204d
Locha <lennart.joehnk@gmail.com>
2024-09-17 10:55:04 +0200

    updated

--- hook-combined.py
+++ hook-combined.py
@@ -1,40 +1,17 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
-# Sammeln Sie alle Submodule von xhtml2pdf und anderen wichtigen Paketen
 hiddenimports = collect_submodules('xhtml2pdf')
 hiddenimports += collect_submodules('PIL')
+hiddenimports += collect_submodules('reportlab')
+hiddenimports=['reportlab.graphics.barcode.code128'],
 hiddenimports += collect_submodules('matplotlib')
-
-# Fügen Sie spezifische Module hinzu, die in Ihrem Projekt verwendet werden
 hiddenimports += [
-    'sqlite3',
-    'logging',
-    'tkinter',
-    'tkinter.ttk',
-    'tkinter.messagebox',
-    'tkinter.filedialog',
-    'webbrowser',
-    'configparser',
-    'base64',
-    'io',
-    'datetime',
-    'os',
-    'shutil',
+    'html5lib', 'pypdf', 'svglib', 'webencodings', 'olefile',
+    'numpy', 'dateutil', 'tzlocal', 'pytz', 'pyparsing',
+    'pkg_resources.py2_warn', 'distutils'
 ]
 
-# Sammeln Sie alle Datendateien von xhtml2pdf und PIL
 datas = collect_data_files('xhtml2pdf')
 datas += collect_data_files('PIL')
-
-# Matplotlib-spezifische Daten
+datas += collect_data_files('reportlab')
 datas += collect_data_files('matplotlib')
-
-# Fügen Sie tkinter-spezifische Daten hinzu (falls nötig)
-# datas += collect_data_files('tkinter')
-
-# Fügen Sie hier spezifische Dateien oder Verzeichnisse hinzu, die Ihr Projekt benötigt
-# Beispiel:
-# datas += [('pfad/zu/ihren/assets', 'assets')]
-
-# Stellen Sie sicher, dass alle benötigten Schriftarten eingebunden sind
-# datas += [('pfad/zu/ihren/fonts', 'fonts')]
